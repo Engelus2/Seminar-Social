fd777e5420d2be45fcadf0f51274d4b636cbdd22
FHecher <florian.hecher@gmail.com>
2017-06-01 12:13:15 -0700

    Bugs fixed. I think

--- FlosdetailedDataCollection.py
+++ FlosdetailedDataCollection.py
@@ -3,9 +3,10 @@ import steamapi
 from steamapi.user import SteamGroup
 import JsonHelper
 import datetime, time
+import json
 
-steamapi.core.APIConnection(api_key="153823DBBAD57AE1360496D35A75FDC0", validate_key=True) 
-#steamapi.core.APIConnection(api_key="3FF94A9C0C8E7833E315736F735CE048", validate_key=True)
+#steamapi.core.APIConnection(api_key="153823DBBAD57AE1360496D35A75FDC0", validate_key=True) 
+steamapi.core.APIConnection(api_key="3FF94A9C0C8E7833E315736F735CE048", validate_key=True)
 
 def setinDictionary(dict, key, value):
     if key in dict:
@@ -17,28 +18,36 @@ def setinDictionary(dict, key, value):
 def goThroughList(list):
     counter = 0
     for x in list:
-	print counter
+	print(counter)
 	counter = counter +1
 	if x not in usedPlayers:
-		     
 		me = steamapi.user.SteamUser(userid = x)
 		if me.privacy == 3:
-		    setinDictionary(userByCountry, me.country_code, me.steamid)
-		    setinDictionary(userByLevel, me.level, me.steamid)
-		    setinDictionary(userByVACBan, me.is_vac_banned, me.steamid)
-		    setinDictionary(userByCommunityBan, me.is_community_banned, me.steamid)
-	 	    setinDictionary(userByLastlogoff, (me.last_logoff-t).total_seconds(), me.steamid)
-	 	    setinDictionary(userByTimecreated, (me.time_created-t).total_seconds(), me.steamid)
+		    if me.country_code is None:
+			setinDictionary(userByCountry, 'null', me.steamid)
+		    else:
+			setinDictionary(userByCountry, me.country_code, me.steamid)
+		    setinDictionary(userByLevel, str(me.level), me.steamid)
+		    if me.is_community_banned is False:
+		    	setinDictionary(userByCommunityBan, 'false', me.steamid)
+		    else:
+		    	setinDictionary(userByCommunityBan, 'true', me.steamid)
+	 	    setinDictionary(userByLastlogoff, str((me.last_logoff-t).total_seconds()), me.steamid)
+	 	    setinDictionary(userByTimecreated, str((me.time_created-t).total_seconds()), me.steamid)
 		    for y in me.games:
 		        setinDictionary(userByGames, y.name, me.steamid)
 		    for y in me.recently_played:
 		        setinDictionary(userByRecentlyplayed, y.name, me.steamid)
 		    for y in me.friends:
-		        setinDictionary(userByFriends, me.steamid, y.steamid)
+		        setinDictionary(userByFriends, str(me.steamid), y.steamid)
+		    if me.is_vac_banned is False:
+		    	setinDictionary(userByVACBan, 'false', me.steamid)
+		    else:
+		    	setinDictionary(userByVACBan, 'true', me.steamid)
 
 def getRidOfAlreadyDone():
-	for i in userByCountry:
-		for x in userByCountry[i]:
+	for i in userByVACBan:
+		for x in userByVACBan[i]:
 			usedPlayers.append(x)
 	
 
@@ -57,6 +66,7 @@ usedPlayers = []
 t = datetime.datetime(1970,1,1)
 
 userBySteamID = JsonHelper.loadJson(userBySteamID, 'PlayerList.txt')
+
 #'''
 userByCountry = JsonHelper.loadJson(userByCountry, 'SortCountry50000.txt')
 userByFriends = JsonHelper.loadJson(userByFriends, 'SortFriend50000.txt')
@@ -73,17 +83,16 @@ try:
     PlayerList = userBySteamID['players']
     getRidOfAlreadyDone()
     goThroughList(PlayerList)
-except:
-    print ('Y')
+except:	
+	print('X')
 
-print('X')
 JsonHelper.printJson(userByFriends, 'SortFriend50000.txt')
 JsonHelper.printJson(userByLevel, 'SortLevel50000.txt')
-JsonHelper.printJson(userByVACBan, 'SortVACBan50000.txt')
 JsonHelper.printJson(userByCommunityBan, 'SortCommunityBan50000.txt')
 JsonHelper.printJson(userByGames, 'SortGames50000.txt')
 JsonHelper.printJson(userByRecentlyplayed, 'SortRecentlyplayed50000.txt')
 JsonHelper.printJson(userByLastlogoff, 'SortLastlogoff50000.txt')
 JsonHelper.printJson(userByTimecreated, 'SortTimecreated50000.txt')
 JsonHelper.printJson(userByCountry, 'SortCountry50000.txt')
+JsonHelper.printJson(userByVACBan, 'SortVACBan50000.txt')
 print('Z')
