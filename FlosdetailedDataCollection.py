6568760d86d8dd200d56d8cd14793fe83f6650f2
FHecher <florian.hecher@gmail.com>
2017-06-02 08:17:13 -0700

    zweite Datenwelle

--- FlosdetailedDataCollection.py
+++ FlosdetailedDataCollection.py
@@ -17,36 +17,48 @@ def setinDictionary(dict, key, value):
 
 def goThroughList(list):
     counter = 0
+    pcounter= 0
+    falsefriends = 0
     for x in list:
-	print(counter)
-	counter = counter +1
-	if x not in usedPlayers:
-		me = steamapi.user.SteamUser(userid = x)
-		if me.privacy == 3:
-			try:
+	try:
+		counter = counter +1
+		if pcounter<100 and x not in usedPlayers:
+			print("total counter: ", counter)
+			me = steamapi.user.SteamUser(userid = x)
+			if me.privacy == 3:
+				pcounter = pcounter + 1
+				print("This time: ", pcounter)
 				if me.country_code is None:
 					setinDictionary(userByCountry, 'null', me.steamid)
-			 	else:
+				else:
 					setinDictionary(userByCountry, me.country_code, me.steamid)
-			 		setinDictionary(userByLevel, str(me.level), me.steamid)
-			 	if me.is_community_banned is False:
-			  		setinDictionary(userByCommunityBan, 'false', me.steamid)
-			 	else:
-			  		setinDictionary(userByCommunityBan, 'true', me.steamid)
-			   	setinDictionary(userByLastlogoff, str((me.last_logoff-t).total_seconds()), me.steamid)
-			   	setinDictionary(userByTimecreated, str((me.time_created-t).total_seconds()), me.steamid)
-		   		for y in me.games:
+				setinDictionary(userByLevel, str(me.level), me.steamid)
+				if me.is_community_banned is False:
+					setinDictionary(userByCommunityBan, 'false', me.steamid)
+				else:
+					setinDictionary(userByCommunityBan, 'true', me.steamid)
+				setinDictionary(userByLastlogoff, str((me.last_logoff-t).total_seconds()), me.steamid)
+				setinDictionary(userByTimecreated, str((me.time_created-t).total_seconds()), me.steamid)
+			   	for y in me.games:
 					setinDictionary(userByGames, y.name, me.steamid)
-			  	for y in me.recently_played:
+				for y in me.recently_played:
 					setinDictionary(userByRecentlyplayed, y.name, me.steamid)
-			  	for y in me.friends:
+				for y in me.friends:
 					setinDictionary(userByFriends, str(me.steamid), y.steamid)
-			  	if me.is_vac_banned is False:
-					setinDictionary(userByVACBan, 'false', me.steamid)
+				if me.is_vac_banned is False:
+				 	setinDictionary(userByVACBan, 'false', me.steamid)
 				else:
 					setinDictionary(userByVACBan, 'true', me.steamid)
-			except:
-				print('Something went wrong')	
+				
+			else:
+				falsefriends = falsefriends + 1
+				print("False Friends: ", falsefriends)
+	except:
+		print('Something went wrong')
+    print("DONE getting data for now")
+    print("total counter: ", counter)
+    print("This time: ", pcounter)
+    print("False Friends: ", falsefriends)
 
 def getRidOfAlreadyDone():
 	for i in userByVACBan:
@@ -98,4 +110,4 @@ JsonHelper.printJson(userByLastlogoff, 'SortLastlogoff50000.txt')
 JsonHelper.printJson(userByTimecreated, 'SortTimecreated50000.txt')
 JsonHelper.printJson(userByCountry, 'SortCountry50000.txt')
 JsonHelper.printJson(userByVACBan, 'SortVACBan50000.txt')
-print('Z')
+print("DONE")
