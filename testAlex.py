ef72d66c077a2f41974e08f3312943f540471ca2
Alexander Engelhard <alexander.engelhard@online.de>
2017-06-25 23:15:42 +0200

    Graph mit attributen

--- testAlex.py
+++ testAlex.py
@@ -1,110 +1,149 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-'''
-Created on 03.05.2017
-
-@author: Alexander
-
-
-
-Kommentare (rechtschreibfehler bitte behalten)
-
-Kurze erklaerung
-stopRequestAt anzahl der nutzer die ihr analysieren wollts
-beim starten:
-    mit eingabe 1 bekommts ihr den fortschritt (gebts eine andere Zahl ein so verschwindert er wieder)
-    mit eingabe 9 beendet ichr das Programm (json wird trotzdem erstellt wie weit er halt gekommen ist)
-
-
-Das ist eine Test klasse also definitiv nicht optimal aber erf�llt den zweck
-
-
-'''
-
-import steamapi
-from steamapi.user import SteamGroup
 import JsonHelper
 import networkx as nx
 import matplotlib.pyplot as plt
-from thread import start_new_thread
-import SaveData
+import time
+
+class User():
+    
+    def __init__(self, id, comBan, country, lastlogOff, level, privacy, recPlayed, timeCreated, vacBan, numFriends, friends):
+        self.id = id
+        self.comBan = comBan
+        self.country = country
+        self.lastlogOff = lastlogOff
+        self.level = level
+        self.privacy = privacy
+        self.recPlayed = recPlayed
+        self.timeCreated = timeCreated
+        self.vacBan = vacBan
+        self.numFriends = numFriends
+        self.friends = friends
+
+def getVariable(dict, id):
+    for key, value in dict.items():
+        if(id in value):
+            return key
+    return 'null'
+
+friends = {}
+friends2 = {}
+comBan = {}
+country = {}
+lastlogOff = {}
+level = {}
+privacy = {}
+recPlayed = {}
+timeCreated = {}
+vacBan = {}
+numFriends = {}
+onlyAnalyzedFriends = {}
+friends = JsonHelper.loadJson(friends, 'FlosData/SortFriend50000.txt')
+friends2 = JsonHelper.loadJson(friends2, 'FlosData/SortFriend50000_2.txt')
+comBan = JsonHelper.loadJson(comBan, 'FlosData/SortCommunityBan50000.txt')
+country = JsonHelper.loadJson(country, 'FlosData/SortCountry50000.txt')
+lastlogOff = JsonHelper.loadJson(lastlogOff, 'FlosData/SortLastlogoff50000.txt')
+level = JsonHelper.loadJson(level, 'FlosData/SortLevel50000.txt')
+privacy = JsonHelper.loadJson(privacy, 'FlosData/SortPrivacy50000.txt')
+recPlayed = JsonHelper.loadJson(recPlayed, 'FlosData/SortRecentlyplayed50000.txt')
+timeCreated = JsonHelper.loadJson(timeCreated, 'FlosData/SortTimecreated50000.txt')
+vacBan = JsonHelper.loadJson(vacBan, 'FlosData/SortVACBan50000.txt')
+numFriends = JsonHelper.loadJson(numFriends, 'FlosData/NumberOfpersonalFriends.txt')
+onlyAnalyzedFriends = JsonHelper.loadJson(onlyAnalyzedFriends, 'onlyAnalyzedFriends.txt')
+
+for x in friends2:
+    friends[x] = friends2[x]
+
+users = {}
+'''
+print users
+c = 0
+for us in friends:
+    c +=1 
+    print us
+    users[us] = User(us, getVariable(comBan,us), getVariable(country,us) , getVariable(lastlogOff,us), getVariable(level,us), getVariable(privacy,us), getVariable(recPlayed,us), getVariable(timeCreated,us), getVariable(vacBan,us), getVariable(numFriends,us), getVariable(friends,us))
+    if c == 10:
+        break;
 
 '''
-Diesen wert setzten Achtung es entspricht nicht der Wirklichen anzahl da manche user doppelt gez�hlt werden
-zB bei 50000 kommen etwa 40000 nutzer raus (wie schon gesagt der code ist eher zweckmae�ig als logisch)
 '''
-stopRequestAt = 500
+def getFriendsConnectionGraph(dictFriends, users):
+    counter = 0.0
+    
+    for x in dictFriends:
+        if(x in users):
+            G.add_node(x, comBan=users[x].comBan, country=users[x].country, lastlogOff=users[x].lastlogOff,
+                        level=users[x].level, privacy=users[x].privacy, recPlayed=users[x].recPlayed, 
+                        timeCreated=users[x].timeCreated, vacBan=users[x].vacBan, numFriends=users[x].numFriends)
+            print counter/len(dictFriends)
+            counter +=1.0
+            for y in dictFriends[x]:
+                if(y in G.nodes()):
+                    G.add_edge(x, y)
+            if(counter == 20):
+                return;
+'''
 
-threadcontroll = 0
-currentrequest = 0 
-steamapi.core.APIConnection(api_key="153823DBBAD57AE1360496D35A75FDC0", validate_key=True) 
-#steamapi.core.APIConnection(api_key="3FF94A9C0C8E7833E315736F735CE048", validate_key=True)  # <-- Insert API key here
-#startuser = steamapi.user.SteamUser(76561197960279154)
-users = [startuser]
-usersByCountry = {}
-userByFriends = {}
+def getTroughDictInGraph(dict, G, name):
+    for key in dict:
+        for id in dict[key]:
+            try:
+                G.node[int(id)][name] = key
+            except:
+                print "need to vrate"
+                G.add_node(int(id))
+                G.node[int(id)][name] = key
+                if key == "DE": 
+                    print "de"
+                    
+
+                    
+
+def getFriendsConnectionGraph(user, G):
+    if str(user) in onlyAnalyzedFriends:
+        for friend in onlyAnalyzedFriends[str(user)]:
+            G.add_edge(int(user), int(friend))
+
+'''
+for user in friends:
+    list = []
+    for friend in friends[str(user)]:
+        if str(friend) in friends:
+            list.append(friend)
+            print "fsa"
+    onlyAnalyzedFriends[user] = list
+JsonHelper.printJson(onlyAnalyzedFriends, 'onlyAnalyzedFriends.txt')
+'''
 
 G = nx.Graph()
 
-'''
-setzt den Wert value in die Liste des Dictionary beim Schluessel key ein
-'''
-def setinDictionary(dict, key, value):
-    if key in dict:
-        if value not in dict[key]:
-            dict[key].append(value)
-    else:
-        dict[key] = [value]
+print "se"
+#G = nx.read_gml("graph.gml")
+#G.add_node(76561197971506367, test='5pm')
+getTroughDictInGraph(comBan, G, 'comBan')
+getTroughDictInGraph(country, G, 'country')
+getTroughDictInGraph(lastlogOff, G, 'lastlogOff')
+getTroughDictInGraph(level, G, 'level')
+getTroughDictInGraph(privacy, G, 'privacy')
+getTroughDictInGraph(recPlayed, G, 'recPlayed')
+getTroughDictInGraph(timeCreated, G, 'timeCreated')
+getTroughDictInGraph(vacBan, G, 'vacBan')
+
+start = time.clock()
+
+counter = 0.0
+anz = len(G.nodes())
+for node in G.nodes():
+    counter +=1
+    getFriendsConnectionGraph(node, G)
+    print counter / anz
+
+print time.clock() - start
+nx.write_gml(G, 'graph.gml')
+print time.clock() - start
 
 '''
-geht ein bisschen verwirrend durch freunde solange bis stopRequesAt angekommen ist
-'''
-def goThrougFriends(user):
-    global currentrequest
-    friendList = user.friends
-    id = user.steamid
-    currentrequest += len(friendList)
-    global threadcontroll
-    for i in friendList:
-        if i not in users:
-            users.append(i)
-            setinDictionary(usersByCountry, i.country_code, i.steamid)
-            setinDictionary(userByFriends, id, i.steamid)
-            if threadcontroll == 1:
-                print len(users)
-                print "find ID " ,currentrequest / float(stopRequestAt)
-                print "sort ID: " ,len(users) / float(currentrequest)
-            if currentrequest < stopRequestAt and i.privacy == 3:
-                goThrougFriends(i)
-    print "ende"  
     
-def getFriendsConnectionGraph(user):
-    if user.privacy == 3:
-        for i in user.friends:
-            if i.privacy == 3:
-                G.add_edge(user.steamid, i.steamid)
-    else:
-        print "notublic"
-
-
-
-
-
-
-# testet die abfrage herausgefunden: es werden teilweise schon noch doppelte abfragen gemacht gerade bei friends(die letzten 2 porint sollten kene abfrage benötigen
-startuser = SaveData.loadIt()
-
-print startuser.name
-print startuser.friends[2].friends
-print startuser.friends
-print startuser.friends[2].friends[0].friends
-JsonHelper.printJson2(startuser, 'test.txt')
-print "ende"
-
-
-
     
-'''       
+         
 getFriendsConnectionGraph(startuser)
 lenge = len(startuser.friends)
 counter = 0
@@ -115,21 +154,4 @@ for x in startuser.friends:
 nx.draw(G)
 plt.show()
 
-start = time.time() 
-
-try:
-    start_new_thread(getFriendsConnectionGraph, (startuser, ))
-
-except:
-    print "vorzeitiges Ende"
-while threadcontroll is not 9:
-    threadcontroll = int(input("Ihre Eingabe? "))
-
-end = time.time()
-# eigentlich nichtssagend  
-print "Time: ", (end - start)
-
-print len(users)
-JsonHelper.printJson(usersByCountry, 'SortCountry.txt')
-JsonHelper.printJson(userByFriends, 'SortFriend.txt')
 '''
